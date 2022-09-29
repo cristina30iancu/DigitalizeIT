5065d0b88cbc9b0c57709cde6e2346b6371394e6
Zugravu Alexandru Stefan <alexandruzugra@gmail.com>
2022-09-29 14:55:54 +0300

    Refresh

--- backend/src/main/java/com/springjwt/repository/NewJoinerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springjwt.repository;
-
-import com.springjwt.models.NewJoiner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewJoinerRepository extends JpaRepository<NewJoiner, Integer> {
-}
