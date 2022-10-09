bb27f09317ec4d91b8b7b15a511903fe1c93d922
Alexandru <alexoporanu11@gmail.com>
2022-10-09 21:18:50 +0300

    Semi-functioning endpoint for assigning equipment to newjoiners

--- backend/src/main/java/com/springjwt/controller/Equipment_newJoinerController.java
+++ backend/src/main/java/com/springjwt/controller/Equipment_newJoinerController.java
@@ -6,10 +6,12 @@ import com.springjwt.models.NewJoiner;
 import com.springjwt.service.Equipment_newJoinerService;
 import com.springjwt.service.NewJoinerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +19,26 @@ import java.util.List;
 public class Equipment_newJoinerController {
 
     private final Equipment_newJoinerService equipment_newJoinerService;
+    //private final equipment_service
     @PutMapping("{newJoinerId}")
-    public List<Equipment_newJoiner> addEquipment_newJoiner(@RequestBody List<Equipment> equipmentList,@PathVariable Long newJoinerId) {
+    //@PreAuthorize("hasRole(ROLE_MANAGER)")
+    public List<Equipment_newJoiner> addEquipment_newJoiner(@RequestBody List<Equipment> equipmentList, @PathVariable Long newJoinerId) {
         return equipment_newJoinerService.addEquipment_newJoiner(equipmentList, newJoinerId);
+
     }
     @GetMapping("all")
+    //@PreAuthorize("hasRole(ROLE_MANAGER)")
     public List<Equipment_newJoiner> getAll() {
         return equipment_newJoinerService.getAll();
+    }
+
+    @GetMapping("equipmentsOfNewjoiner/{newJoinerId}")
+    //@PreAuthorize("hasRole(ROLE_MANAGER)")
+    public List<Equipment> getEquipmentsOfNewJoiner(@PathVariable Long newJoinerId) {
+        List<Equipment_newJoiner> all = equipment_newJoinerService.getAll();
+        List<Equipment> toReturn = all.stream().
+                        filter(en -> en.getNewJoiner().getId() == newJoinerId).
+                        map(en -> en.getEquipment()).collect(Collectors.toList());
+        return toReturn;
     }
 }
