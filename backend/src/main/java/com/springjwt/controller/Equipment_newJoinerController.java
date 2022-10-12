e39ba455782920396edab3fd9d383a58844c295a
nastrutdiana <diana.nastrut1@gmail.com>
2022-10-12 22:15:15 +0300

    :)

--- backend/src/main/java/com/springjwt/controller/Equipment_newJoinerController.java
+++ backend/src/main/java/com/springjwt/controller/Equipment_newJoinerController.java
@@ -22,7 +22,7 @@ public class Equipment_newJoinerController {
     //private final equipment_service
     @PutMapping("{newJoinerId}")
     //@PreAuthorize("hasRole(ROLE_MANAGER)")
-    public List<Equipment_newJoiner> addEquipment_newJoiner(@RequestBody List<Integer> equipmentIdsList, @PathVariable Long newJoinerId) {
+    public List<Equipment_newJoiner> addEquipment_newJoiner(@RequestBody List<Long> equipmentIdsList, @PathVariable Long newJoinerId) {
         return equipment_newJoinerService.addEquipment_newJoiner(equipmentIdsList, newJoinerId);
     }
     @GetMapping("all")
--- backend/src/main/java/com/springjwt/service/Equipment_newJoinerService.java
+++ backend/src/main/java/com/springjwt/service/Equipment_newJoinerService.java
@@ -20,14 +20,14 @@ public class Equipment_newJoinerService {
     private final NewJoinerRepository newJoinerRepository;
     private final EquipmentRepository equipmentRepository;
 
-    public List<Equipment_newJoiner> addEquipment_newJoiner(List<Integer> equipmentIdsList, Long newJoinerID) {
+    public List<Equipment_newJoiner> addEquipment_newJoiner(List<Long> equipmentIdsList, Long newJoinerID) {
 
         NewJoiner newJoiner = newJoinerRepository.findById(newJoinerID);
 
         List<Equipment_newJoiner> toReturn = new ArrayList<>();
 
         for(int i = 0; i < equipmentIdsList.size(); ++i) {
-            Equipment currEquipment = equipmentRepository.findById(equipmentIdsList.get(i));
+            Equipment currEquipment = equipmentRepository.findById(equipmentIdsList.get(i)).get();
             Equipment_newJoiner equipment_newJoiner = new Equipment_newJoiner(null,false, currEquipment, newJoiner);
             toReturn.add(equipment_newJoiner);
             equipment_newJoinerRepository.save(equipment_newJoiner);
