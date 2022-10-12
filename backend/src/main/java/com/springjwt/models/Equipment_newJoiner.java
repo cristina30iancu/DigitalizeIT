be53bbafa0764b21d727bf7c247e59df2641888c
nastrutdiana <diana.nastrut1@gmail.com>
2022-10-12 18:05:01 +0300

    done

--- backend/src/main/java/com/springjwt/models/Equipment_newJoiner.java
+++ backend/src/main/java/com/springjwt/models/Equipment_newJoiner.java
@@ -1,11 +1,15 @@
 package com.springjwt.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Equipment_newJoiner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ backend/src/main/java/com/springjwt/repository/Equipment_newJoinerRepository.java
@@ -0,0 +1,11 @@
+package com.springjwt.repository;
+
+import com.springjwt.models.Equipment_newJoiner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface Equipment_newJoinerRepository extends JpaRepository<Equipment_newJoiner, Long> {
+    List<Equipment_newJoiner> findAllByNewJoinerId(Long id);
+    Equipment_newJoiner findByNewJoinerIdAndAndEquipmentId(Long idJoiner, Long idEquipment);
+}
